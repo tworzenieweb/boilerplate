d1a6d28f229797037b498aa41d5873f17af26618
Luke Adamczewski <tworzenieweb@gmail.com>
2015-08-05 23:24:37 +0200

    Initial commit for project skeleton

---
+++ app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This app require composer for autoloading and installing components
+ * Remember to run `composer install` first
+ * Enter point for whole app - tie it up together here
+ */
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+
+require_once __DIR__.'/vendor/autoload.php';
